230af9f910265daef3aa33152af849ce9e11a86b
unknown <uxiomercur@gmail.com>
2024-03-28 14:24:43 +0100

    Cambios cos novos datos 23/24

--- carga_inicial.R
+++ carga_inicial.R
@@ -6,15 +6,15 @@ library(tidyr)
 df <- data.frame(matrix(ncol = 0, nrow = 330))
 # Liga ACB 2023/24
 tfg <- read_csv("tfg.csv")
-tfg1 <- read_csv("tfg (1).csv")
-tfg2 <- read_csv("tfg (2).csv")
+tfg1 <- read_csv("PPMin.csv")
+tfg2 <- read_csv("PPPos.csv")
 boxscore <- read_csv("boxscore.csv")
 head(boxscore)
 bs <- boxscore[, c(3, 23:40)]
 PPT <- read_csv("PPt.csv")
 PPT2 <- read_csv("PPT2.csv")
 PPT3 <- read_csv("PPT3.csv")
-TCInt <- read_csv("%TC.csv")
+TCInt <- read_csv("%TCInt.csv")
 T2Inte <- read_csv("%T2Int.csv")
 T3Int <- read_csv("%T3Int.csv")
 Ptos <- read_csv("%Ptos.csv")
@@ -23,11 +23,11 @@ Ptos3 <- read_csv("%3Ptos.csv")
 TAsist <- read_csv("%TAsist.csv")
 T2Asist <- read_csv("%T2Asist.csv")
 T3Asist <- read_csv("%T3Asist.csv")
-TL_Min <- read_csv("TL-min.csv")
+TL_Min <- read_csv("TL_min.csv")
 TL_F <- read_csv("TL_F.csv")
-RebTot <- read_csv("RTot.csv")
-RebOf <- read_csv("RebOf.csv")
-RebDef <- read_csv("RebDef.csv")
+RebTot <- read_csv("%Reb.csv")
+RebOf <- read_csv("%RebOf.csv")
+RebDef <- read_csv("%RebDef.csv")
 Asist <- read_csv("%Asist.csv")
 TO <- read_csv("%TO.csv")
 LE <- read_csv("LE.csv")
@@ -74,7 +74,7 @@ df$"LE" <- LE$Media
 save(df, file="df.RData")
 
 
-df3 <- data.frame(matrix(ncol = 0, nrow = 284))
+df3 <- data.frame(matrix(ncol = 0, nrow = 313))
 # LigaACB 2023/24
 PPP2 <- read_csv("PPP2.csv")
 PPMin2 <- read_csv("PPMin2.csv")
@@ -90,7 +90,7 @@ T3Int2 <- read_csv("%T3Abs2.csv")
 Ptos2 <- read_csv("%Ptos2.csv")
 Ptos22 <- read_csv("%2Ptos2.csv")
 Ptos32 <- read_csv("%3Ptos2.csv")
-TAsist2 <- read_csv("%Asist2.csv")
+TAsist2 <- read_csv("%TAsist2.csv")
 T2Asist2 <- read_csv("%T2Asist2.csv")
 T3Asist2 <- read_csv("%T3Asist2.csv")
 TL_Min2 <- read_csv("TL_Min2.csv")
@@ -98,7 +98,7 @@ TL_F2 <- read_csv("TL_F2.csv")
 RebTot2 <- read_csv("%Reb2.csv")
 RebOf2 <- read_csv("%RebOf2.csv")
 RebDef2 <- read_csv("%RebDef2.csv")
-Asist2 <- read_csv("%Asistencias2.csv")
+Asist2 <- read_csv("%Asist2.csv")
 TO2 <- read_csv("%TO2.csv")
 LE2 <- read_csv("LE2.csv")
 
@@ -145,123 +145,3 @@ save(df3, file="df3.RData")
 
 colnames(df)
 colnames(df3)
-
-
-setwd("~/GCED/TFG/Datos")
-
-PPP_eq <- read_csv("PPP_equipo.csv")
-PPMin_eq <- read_csv("PPMin_equipo.csv")
-PPPos_eq <- read_csv("PPPos_eq.csv")
-PPT_eq <- read_csv("PPT_eq.csv")
-PPT2_eq <- read_csv("PPT2_eq.csv")
-PPT3_eq <- read_csv("PPT3_eq.csv")
-Asist <- read_csv("%Asist_eq.csv")
-AsistT2 <- read_csv("%AsistT2_eq.csv")
-AsistT3 <- read_csv("%AsistT3_eq.csv")
-TL_Min <- read_csv("TL_Min_eq.csv")
-TL_F <- read_csv("TL_F_eq.csv")
-RebTot <- read_csv("%Reb_eq.csv")
-RebOf <- read_csv("RebOf_eq.csv")
-RebDef <- read_csv("RebDef_eq.csv")
-
-
-
-
-PPP_LE <- read_csv("PPP_LE.csv")
-PPMin_LE <- read_csv("PPMin_LE.csv")
-PPPos_LE <- read_csv("PPPos_LE.csv")
-PPT_LE <- read_csv("PPT_LE.csv")
-PPT2_LE <- read_csv("PPT2_LE.csv")
-PPT3_LE <- read_csv("PPT3_LE.csv")
-Asist_LE <- read_csv("%TAsist_LE.csv")
-AsistT2_LE <- read_csv("%T2Asist_LE.csv")
-AsistT3_LE <- read_csv("%T3Asist_LE.csv")
-TL_Min_LE <- read_csv("TL_Min_LE.csv")
-TL_F_LE <- read_csv("TL_F_LE.csv")
-RebTot_LE <- read_csv("%Reb_LE.csv")
-RebOf_LE <- read_csv("%RebOf_LE.csv")
-RebDef_LE <- read_csv("%RebDef_LE.csv")
-
-
-funcional <- function(df){
-  df <- df %>%  filter(Xornada != 0)        
-  df <- subset(df, select = c(Equipo, Xornada, `Valor Equipo`))
-  return( df %>%
-    spread(key = Xornada, value = `Valor Equipo`))
-}
-
-limpieza <- function(df){
-  df <- df %>%  filter(Xornada == 0)        
-  df <- subset(df, select = c(Equipo, `Media Equipo`))
-  return(df)
-}
-
-df_equipos <- data.frame()
-
-PPP_LE <- limpieza(PPP_LE)
-PPMin_LE <- limpieza(PPMin_LE)
-PPPos_LE <- limpieza(PPPos_LE)
-PPT_LE <- limpieza(PPT_LE)
-PPT2_LE <- limpieza(PPT2_LE)
-PPT3_LE <- limpieza(PPT3_LE)
-Asist_LE <- limpieza(Asist_LE)
-AsistT2_LE <- limpieza(AsistT2_LE)
-AsistT3_LE <- limpieza(AsistT3_LE)
-TL_Min_LE <- limpieza(TL_Min_LE)
-TL_F_LE <- limpieza(TL_F_LE)
-RebTot_LE <- limpieza(RebTot_LE)
-RebOf_LE <- limpieza(RebOf_LE)
-RebDef_LE <- limpieza(RebDef_LE)
-
-
-df <- data.frame(Equipo = RebDef_LE$Equipo)
-lista_variables <- list(PPP_LE, PPMin_LE, PPPos_LE, PPT_LE, PPT2_LE, PPT3_LE, Asist_LE, 
-                        AsistT2_LE, AsistT3_LE, TL_Min_LE, TL_F_LE, RebTot_LE, RebOf_LE,
-                        RebDef_LE)
-
-for (df_intermedio in lista_variables) {
-  df <- left_join(df, df_intermedio, by = "Equipo")
-}
-
-colnames(df) <- list('Equipos', 'PPP', 'PPMin', 'PPPos', 'PPT', 'PPT2', 
-                     'PPT3', 'Asist', 'AsistT2', 'AsistT3', 'TL_Min',
-                     'TL_F', 'RebTot', 'RebOf', 'RebDef')
-
-
-
-
-PPP_eq <- limpieza(PPP_eq)
-PPMin_eq <- limpieza(PPMin_eq)
-PPPos_eq <- limpieza(PPPos_eq)
-PPT_eq <- limpieza(PPT_eq)
-PPT2_eq <- limpieza(PPT2_eq)
-PPT3_eq <- limpieza(PPT3_eq)
-Asist <- limpieza(Asist)
-AsistT2 <- limpieza(AsistT2)
-AsistT3 <- limpieza(AsistT3)
-TL_Min <- limpieza(TL_Min)
-TL_F <- limpieza(TL_F)
-RebTot <- limpieza(RebTot)
-RebOf <- limpieza(RebOf)
-RebDef <- limpieza(RebDef)
-
-
-
-lista_variables2 <- list(PPP_eq, PPMin_eq, PPPos_eq, PPT_eq, PPT2_eq, PPT3_eq, Asist, 
-                         AsistT2, AsistT3, TL_Min, TL_F, RebTot, RebOf, RebDef)
-
-df2 <- data.frame(Equipo = RebDef$Equipo)
-for (df_intermedio in lista_variables2) {
-  df2 <- left_join(df2, df_intermedio, by = "Equipo")
-}
-
-colnames(df2) <- list('Equipos', 'PPP', 'PPMin', 'PPPos', 'PPT', 'PPT2', 
-                     'PPT3', 'Asist', 'AsistT2', 'AsistT3', 'TL_Min',
-                     'TL_F', 'RebTot', 'RebOf', 'RebDef')
-
-
-df_equipos <- rbind(df, df2)
-
-
-save(df_equipos, file = "df_equipos.RData")
-
